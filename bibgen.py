8f23e4dea79a26ba4f80beb08728c0010c2cd972
Quentin DUCASSE <quentin.ducasse@ensta-bretagne.org>
2021-12-14 17:59:29 +0100

    bib generator

---
+++ bibgen.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import sys
+import os
+import PySimpleGUI as sg
+
+sg.theme('light brown 8')
+
+# Base64 versions of images of a folder and a file. PNG files (may not work with PySimpleGUI27, swap with GIFs)
+
+folder_icon = b'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsSAAALEgHS3X78AAABnUlEQVQ4y8WSv2rUQRSFv7vZgJFFsQg2EkWb4AvEJ8hqKVilSmFn3iNvIAp21oIW9haihBRKiqwElMVsIJjNrprsOr/5dyzml3UhEQIWHhjmcpn7zblw4B9lJ8Xag9mlmQb3AJzX3tOX8Tngzg349q7t5xcfzpKGhOFHnjx+9qLTzW8wsmFTL2Gzk7Y2O/k9kCbtwUZbV+Zvo8Md3PALrjoiqsKSR9ljpAJpwOsNtlfXfRvoNU8Arr/NsVo0ry5z4dZN5hoGqEzYDChBOoKwS/vSq0XW3y5NAI/uN1cvLqzQur4MCpBGEEd1PQDfQ74HYR+LfeQOAOYAmgAmbly+dgfid5CHPIKqC74L8RDyGPIYy7+QQjFWa7ICsQ8SpB/IfcJSDVMAJUwJkYDMNOEPIBxA/gnuMyYPijXAI3lMse7FGnIKsIuqrxgRSeXOoYZUCI8pIKW/OHA7kD2YYcpAKgM5ABXk4qSsdJaDOMCsgTIYAlL5TQFTyUIZDmev0N/bnwqnylEBQS45UKnHx/lUlFvA3fo+jwR8ALb47/oNma38cuqiJ9AAAAAASUVORK5CYII='
+file_icon = b'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsSAAALEgHS3X78AAABU0lEQVQ4y52TzStEURiHn/ecc6XG54JSdlMkNhYWsiILS0lsJaUsLW2Mv8CfIDtr2VtbY4GUEvmIZnKbZsY977Uwt2HcyW1+dTZvt6fn9557BGB+aaNQKBR2ifkbgWR+cX13ubO1svz++niVTA1ArDHDg91UahHFsMxbKWycYsjze4muTsP64vT43v7hSf/A0FgdjQPQWAmco68nB+T+SFSqNUQgcIbN1bn8Z3RwvL22MAvcu8TACFgrpMVZ4aUYcn77BMDkxGgemAGOHIBXxRjBWZMKoCPA2h6qEUSRR2MF6GxUUMUaIUgBCNTnAcm3H2G5YQfgvccYIXAtDH7FoKq/AaqKlbrBj2trFVXfBPAea4SOIIsBeN9kkCwxsNkAqRWy7+B7Z00G3xVc2wZeMSI4S7sVYkSk5Z/4PyBWROqvox3A28PN2cjUwinQC9QyckKALxj4kv2auK0xAAAAAElFTkSuQmCC'
+
+starting_path = "./Articles"
+
+treedata = sg.TreeData()
+
+def add_files_in_folder(parent, dirname):
+    files = sorted(os.listdir(dirname))
+    for f in files:
+        fullname = os.path.join(dirname, f)
+        if os.path.isdir(fullname):            # if it's a folder, add folder and recurse
+            treedata.Insert(parent, fullname, f, values=[], icon=folder_icon)
+            add_files_in_folder(fullname, fullname)
+        else:
+            treedata.Insert(parent, fullname, f, values=[os.stat(fullname).st_size], icon=file_icon)
+
+add_files_in_folder('', starting_path)
+
+layout = [[sg.Tree(data=treedata,
+                   headings=['Size', ],
+                   auto_size_columns=True,
+                   select_mode=sg.TABLE_SELECT_MODE_EXTENDED,
+                   num_rows=20,
+                   col0_width=40,
+                   key='-TREE-',
+                   show_expanded=False,
+                   enable_events=True)]]
+
+window = sg.Window('Select articles to add to the bib', layout, resizable=True, finalize=True)
+window['-TREE-'].expand(True, True) # Resize with the window (Full support for Tree element being released in 4.44.0)
+
+while True:     # Event Loop
+    event, values = window.read()
+
+    bib = []
+    for folder in values['-TREE-']:
+        with open(folder + "/custombib.bib","r") as f:
+            bib += f.readlines()
+
+    with open("biblio.bib", "w") as f:
+        f.writelines(bib)
+
+    if event in (sg.WIN_CLOSED, 'Cancel'):
+        break
+window.close()
